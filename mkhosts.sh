ac3845c3fedb52dea27c54e273b53193a400066a
XuanStar <w991593239@163.com>
2018-02-19 21:09:55 +0800

    修改判断是否ping成功的逻辑

--- mkhosts.sh
+++ mkhosts.sh
@@ -1,19 +1,21 @@
 #!/bin/sh
 if [ -f "hosts" ];then
-   mv hosts "hosts_`stat -c %Y hosts`"
+    mv hosts "hosts_`stat -c %Y hosts`"
 fi
 if [ -f "faild" ];then
-   mv faild "faild_`stat -c %Y faild`"
+    mv faild "faild_`stat -c %Y faild`"
 fi
 for url in `cat $PWD/url.dat`
 do
 #out=`ping -c 1 $url | grep -o -P "(\d+\.)(\d+\.)(\d+\.)\d+" | sed -n 1p`
-out=`nslookup $url|grep -o -P "(\d+\.)(\d+\.)(\d+\.)\d+"|sed -n 3p`
-if [ `echo "$out"| grep -c '$url'` -eq 0 ];then
-   echo $out $url
-   echo $out $url >> $PWD/hosts
+do=`nslookup $url`
+#out=`echo $do|grep -o -P "(\d+\.)(\d+\.)(\d+\.)\d+"|sed -n 3p`
+if [ `echo "$do"|grep -i "can't">/dev/null;echo $?` -ne 0 ];then
+    out=`echo $do|grep -o -P "(\d+\.)(\d+\.)(\d+\.)\d+"|sed -n 3p`
+    echo $out $url
+    echo $out $url >> $PWD/hosts
 else
-   echo ping $url fail
-   echo $url >>  $PWD/faild
+    echo ping $url fail
+    echo $url >>  $PWD/faild
 fi
 done
